abaee5be15eb5c0642ccd8a1b268d0360c8526b5
KoenPasteels <kpasteels@hotmail.com>
2023-04-20 19:32:32 +0200

    almost celsius-binding

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -25,38 +25,16 @@ namespace View
             InitializeComponent();
         }
 
-        private void ConvertCelsius(object sender, RoutedEventArgs e)
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            var text = double.Parse(textBox.Text);
-            var fahrenheit = (text * 1.8) + 32;
-            var kelvin = text + 273.15;
+            var value = slider.Value;
 
-            var rFahrenheit = fahrenheit.ToString();
-            var rKelvin = kelvin.ToString();
-            textBox2.Text = rFahrenheit;
-            textBox3.Text = rKelvin;
-        }
-
-        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
-        {
-            var text = double.Parse(textBox2.Text);
-            var celsius = (text - 32) / 1.8;
-            var kelvin = celsius + 273.15;
-
-            var rCelsius = celsius.ToString();
-            var rKelvin = kelvin.ToString();
-            textBox.Text = rCelsius;
-            textBox3.Text = rKelvin;
-        }
-
-        private void ConvertKelvin(object sender, RoutedEventArgs e)
-        {
-            var text = double.Parse(textBox3.Text);
-            var celsius = text - 273.15;
+            var celsius = value - 273.15;
             var fahrenheit = (celsius * 1.8) + 32;
 
             var rCelsius = celsius.ToString();
             var rFahrenheit = fahrenheit.ToString();
+
             textBox.Text = rCelsius;
             textBox2.Text = rFahrenheit;
         }
