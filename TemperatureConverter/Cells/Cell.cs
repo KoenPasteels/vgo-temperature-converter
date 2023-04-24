5c6e417e57e17f0fc65fb36a2998c3ac0fe58912
KoenPasteels <kpasteels@hotmail.com>
2023-04-24 20:37:55 +0200

    derived-set

--- TemperatureConverter/Cells/Cell.cs
+++ TemperatureConverter/Cells/Cell.cs
@@ -16,7 +16,7 @@ namespace Cells
             this.contents = initialContents;
         }
 
-        public T Value
+        public virtual T Value
         {
             get
             {
@@ -30,6 +30,39 @@ namespace Cells
             }
         }
 
+        public Cell<U> Derive<U>(Func<T, U> transformer, Func<U, T> untransformer)
+        {
+            return new Derived<T, U>(this, transformer, untransformer);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Derived<IN, OUT> : Cell<OUT>
+    {
+        private readonly Cell<IN> dependency;
+        private readonly Func<IN, OUT> transformer;
+        private readonly Func<OUT, IN> untransformer;
+
+        public Derived(Cell<IN> dependency, Func<IN, OUT> transformer, Func<OUT, IN> untransformer) : base(transformer(dependency.Value))
+        {
+            this.dependency = dependency;
+            this.transformer = transformer;
+            this.untransformer = untransformer;
+
+            this.dependency.PropertyChanged += (sender, args) => base.Value = transformer(dependency.Value);
+        }
+
+        public override OUT Value
+        {
+            get
+            {
+                return base.Value;
+            }
+            set
+            {
+                this.dependency.Value = untransformer(value);
+            }
+        }
     }
 }
